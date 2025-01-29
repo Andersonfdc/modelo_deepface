4be12277eb6699a9e35e4d0366891e9f19183efb
Anderson_Costa <81028954+Andersonfdc@users.noreply.github.com>
2025-01-29 12:35:37 +0000

    debug

--- app.py
+++ app.py
@@ -413,6 +413,8 @@ if uploaded_file or captured_image:
         with st.spinner("Classificando a imagem..."):
             resultado = classify_image(uploaded_file.name)
             st.success(f"A imagem pertence à classe: {resultado}")
+            saved_path = save_image(uploaded_file.name, resultado)
+            st.write(f"Imagem salva em: {saved_path}")
             
     except Exception as e:
         st.error(f"Erro durante a classificação: {e}")
@@ -496,11 +498,16 @@ def occlusion_map(image_path, block_size, stride, model_path='face_recognition_m
 
 if uploaded_file and st.button("Gerar Mapa de Oclusão"):
     with st.spinner("Gerando mapa de oclusão...O tempo irá variar de acordo com o tamanho da imagem"):
+        path = os.path.join(train_dir,resultado,uploaded_file.name)
+        st.write(f"Imagem salva em: {path}")
         try:
             path = os.path.join(train_dir,resultado,uploaded_file.name)
+            st.write(f"Imagem salva em: {path}")
             block_size, stride = get_occlusion_params(path)
             occlusion_map("temp_test_image.jpg",block_size, stride)
         except ValueError as e:
+            path = os.path.join(train_dir,resultado,uploaded_file.name)
+            st.write(f"Imagem salva em: {path}")
             print(e)
             st.warning("Não foi possível carregar a imagem 🙁.")
             col1, col2, col3 = st.columns([1, 2, 1])  # Ajuste a proporção conforme necessário
